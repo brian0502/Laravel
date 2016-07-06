97dc948b570fe9d98eff56a4620714862e5746de
BrianPan <aaaaapp@hotmail.com.tw>
2016-07-06 15:21:35 +0800

    project view bug fix

--- resources/views/home/project.blade.php
+++ resources/views/home/project.blade.php
@@ -7,11 +7,11 @@
 				@foreach($data as $key => $val)
 					<a class="grid__item" href="{{ $val['url'] }}" target="_blank">
 						<?php echo empty($val['img'])? '':'<img src="/project_img/'.$val['img'].'" style="width: 100%;">';?>
-						<!-- <h2 class="title title--preview">{{ $val['project_name'] }}</h2> -->
+						<!-- <h2 class="title title--preview">{{ $val['name'] }}</h2> -->
 						<div class="loader"></div>
 						<span class="category">{{ $val['name'] }}</span>
 						<!-- <div class="meta meta--preview">
-							<img class="meta__avatar" src="<?php echo '/project_img/'.$val['project_img'];?>" alt="author01" />
+							<img class="meta__avatar" src="<?php echo '/project_img/'.$val['img'];?>" alt="author01" />
 						</div> -->
 					</a>
 				@endforeach
